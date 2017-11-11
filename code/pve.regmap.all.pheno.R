435cb5b9c97a263f443c6afc6d6c3ceb304e70d0
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-11-11 16:41:58 -0600

    I have a first draft of the pve.regmap.all.pheno.R for running multiple PVE analyses via SLURM.

---
+++ code/pve.regmap.all.pheno.R
@@ -0,0 +1,19 @@
+# This script runs the PVE analysis for all phenotypes.
+
+# LOAD REGMAP DATA
+# ----------------
+cat("Loading RegMap data.\n")
+load("../data/regmap.RData")
+
+# ESTIMATE PVE IN ALL PHENOTYPES
+# ------------------------------
+phenotypes <- names(regmap.pheno)
+cat("Submitting SLURM jobs for PVE analyses in RegMap data:\n")
+for (phenotype in phenotypes) {
+  cat(" - ",phenotype,": ",sep="")
+  system(sprintf(paste("sbatch --job-name=%s",
+                       "--output=../output/pve_regmap_%s.out",
+                       "--error=../output/pve_regmap_%s.err",
+                       "pve_regmap.sbatch %s 1"),
+                 phenotype,phenotype,phenotype,phenotype))
+}
