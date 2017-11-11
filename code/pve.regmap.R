c5976322bd80a26a372950b377ec88cb4526112d
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-11-11 16:09:22 -0600

    Added Rscript functionality to pve.regmap.R.

--- code/pve.regmap.R
+++ code/pve.regmap.R
@@ -29,12 +29,30 @@ source("functions.R")
 
 # SCRIPT PARAMETERS
 # -----------------
-# The computation of the sample weights is divided among this many
-# threads.
-nc <- 8
+# To run this script using Rscript (or "batch mode"), type the
+# following in the command line:
+#
+#   Rscript eval.geno.impute.R <phenotype> <nc>
+#
+# where <phenotype> is the phenotype to analyze, and <nc> is the
+# number of threads to use for computing the weights.
+args <- commandArgs(trailingOnly = TRUE)
 
-# Phenotype to analyze.
-phenotype <- "bio4_temp_season"
+# Phenotype to analyze. The default is to analyze the Temperature
+# Seasonality phenotype.
+if (length(args) < 1) {
+  phenotype <- "bio4_temp_season"
+} else {
+  phenotype <- args[1]
+}
+
+# The computation of the sample weights is divided among this many
+# threads. The default is to use a single thread.
+if (length(args) < 2) {
+  nc <- 1
+} else {
+  nc <- as.integer(args[2])
+}
 
 # Candidate values for the PVE parameter.
 h <- seq(0.001,0.999,0.001)
@@ -80,6 +98,9 @@ cat("Computation took",timing.kinship["elapsed"],"seconds.\n")
 # log-weight for each PVE parameter setting.
 library(parallel)
 cat("Computing weights for",length(h),"settings of PVE parameter.\n")
+cat("Weights are being computed separately in",nc,"threads.\n")
+cat("Number of threads used for BLAS operations:",
+    Sys.getenv("OPENBLAS_NUM_THREADS"),"\n")
 timing.weights <-
   system.time(logw <- compute.log.weights.multicore(K,y,sa,nc = nc))
 cat("Computation took",timing.weights["elapsed"],"seconds.\n")
