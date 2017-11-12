52858551b2a3b1a844f52c78778692c4690b4aa3
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-11-12 14:16:46 -0600

    Moduralized compile.regmap.R code.

--- code/compile.regmap.R
+++ code/compile.regmap.R
@@ -6,6 +6,7 @@
 #
 library(methods)
 library(data.table)
+source("functions.R")
 
 # LOAD SAMPLE INFO
 # ----------------
@@ -14,58 +15,27 @@ library(data.table)
 # label, and country label. This should produce a data frame with
 # 1,307 rows (samples) and 6 columns.
 cat("Reading sample info.\n")
-regmap.info <- read.table("../data/call_method_75_info.tsv",sep = "\t",
-                      header = TRUE,as.is = c("nativename","region"),
-                      quote = "",comment = "#")
-ids         <- regmap.info$array_id
-regmap.info <- regmap.info[c("ecotype_id","median_intensity","latitude",
-                             "longitude","region","country")]
-rownames(regmap.info) <- ids
-
-# If the region is the empty string, set it to missing. Then set this
-# column to a factor.
-rows                       <- which(regmap.info$region == "")
-regmap.info[rows,"region"] <- NA
-regmap.info                <- transform(regmap.info,region = factor(region))
+regmap.info <- read.regmap.info("../data/call_method_75_info.tsv")
 
 # LOAD PHENOTYPE DATA
 # -------------------
 # This should produce a data frame, "regmap.pheno" , with 948 rows
 # (samples) and 48 columns (phenotypes).
-out <- read.table("../data/allvars948_notnormd_011311.txt",sep = "\t",
-                  header = TRUE,comment = "#",quote = "",
-                  stringsAsFactors = FALSE,check.names = FALSE)
-regmap.pheno        <- as.data.frame(t(out[-1]))
-names(regmap.pheno) <- out$phenotype
-
+cat("Reading phenotype data.\n")
+regmap.pheno <- read.regmap.pheno("../data/allvars948_notnormd_011311.txt")
+  
 # LOAD GENOTYPE DATA
 # ------------------
 # Load the genotype data and information about the genetic markers.
-
 cat("Reading genotype data.\n")
-out <- fread("../data/call_method_75_TAIR9.csv",sep = ",",header = TRUE,
-             stringsAsFactors = FALSE,verbose = FALSE,
-             showProgress = FALSE)
-class(out)     <- "data.frame"
-out            <- out[-1,]
-regmap.markers <- out[,1:2]
-names(regmap.markers) <- c("chr","pos")
-regmap.markers <- transform(regmap.markers,
-                            chr = factor(chr),
-                            pos = as.numeric(pos))
-out <- t(out[,-(1:2)])
+out <- read.regmap.geno("../data/call_method_75_TAIR9.csv")
+regmap.markers <- out$markers
+regmap.geno    <- out$geno
+rm(out)
 
 # Convert the genotype data to a binary matrix.
 cat("Converting genotype data to a binary matrix.\n")
-n           <- nrow(out)
-p           <- ncol(out)
-regmap.geno <- matrix(0,n,p)
-ids         <- rownames(out)
-rownames(regmap.geno) <- ids
-for (i in 1:p) {
-  a               <- names(which.min(table(factor(out[,i]))))
-  regmap.geno[,i] <- out[,i] == a
-}
+regmap.geno <- regmap.geno.as.binary(regmap.geno)
 
 # Reorder the rows of regmap.info so that they match up with the rows
 # of regmap.geno.
--- code/functions.R
+++ code/functions.R
@@ -1,3 +1,64 @@
+# Read from a text file the following info on the RegMap samples:
+# array id, ecotype id, genotyping intensity, geographic co-ordinates
+# (latitude and longitude), region label, and country label. This
+# should produce a data frame with 1,307 rows (samples) and 6 columns.
+read.regmap.info <- function (filename) {
+  out <- read.table(filename,sep = "\t",header = TRUE,quote = "",
+                    comment = "#",as.is = c("nativename","region"))
+  ids <- out$array_id
+  out <- out[c("ecotype_id","median_intensity","latitude","longitude",
+               "region","country")]
+  rownames(out) <- ids
+
+  # If the region is the empty string, set it to missing. Then set
+  # this column to a factor.
+  rows <- which(out$region == "")
+  out[rows,"region"] <- NA
+  return(transform(out,region = factor(region)))
+}
+
+# Read the RegMap phenotype data from a text file. This should produce
+# a data frame with 948 rows (samples) and 48 columns (phenotypes).
+read.regmap.pheno <- function (filename) {
+  out <- read.table(filename,sep = "\t",header = TRUE,comment = "#",
+                    quote = "",stringsAsFactors = FALSE,check.names = FALSE)
+  out <- as.data.frame(t(out[-1]))
+  names(out) <- out$phenotype
+  return(out)
+}
+
+# Read the RegMap genotype data and marker information from a CSV file.
+read.regmap.geno <- function (filename) {
+  out <- fread(filename,sep = ",",header = TRUE,stringsAsFactors = FALSE,
+               verbose = FALSE,showProgress = FALSE)
+  class(out)     <- "data.frame"
+  out            <- out[-1,]
+  markers        <- out[,1:2]
+  names(markers) <- c("chr","pos")
+  markers        <- transform(markers,
+                              chr = factor(chr),
+                              pos = as.numeric(pos))
+  geno           <- t(geno[,-(1:2)])
+  return(list(markers = markers,geno = geno))
+}
+
+# Convert the RegMap genotype data to a binary matrix.
+regmap.geno.as.binary <- function (dat) {
+  n   <- nrow(dat)
+  p   <- ncol(dat)
+  out <- matrix(0,n,p)
+  ids <- rownames(dat)
+  rownames(out) <- ids
+
+  # Repeat for each column (i.e., genetic variant).
+  for (i in 1:p) {
+    a       <- names(which.min(table(factor(dat[,i]))))
+    out[,i] <- dat[,i] == a
+  }
+  
+  return(out)
+}
+
 # Create a scatterplot from two selected columns (x, y) in data
 # frame (dat).
 generate.scatterplot <- function (dat, x, y)
