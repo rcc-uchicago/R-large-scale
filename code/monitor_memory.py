9b0bb1dbbf9f6152e3fb1bbc69ec6deb713f3c6b
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-10-28 14:08:14 -0500

    Update monitor_memory.py.

--- code/monitor_memory.py
+++ code/monitor_memory.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
+#
 # Copyright (c) 2012 Realz Slaw, 2017 Gao Wang
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
 import time
@@ -52,35 +55,39 @@ class ProcessTimer:
     try:
       pp = psutil.Process(self.p.pid)
 
-      #obtain a list of the subprocess and all its descendants
+      # Obtain a list of the subprocess and all its descendants.
       descendants = list(pp.children(recursive=True))
       descendants = descendants + [pp]
 
       rss_memory = 0
       vms_memory = 0
 
-      #calculate and sum up the memory of the subprocess and all its descendants
+      # Calculate and sum up the memory of the subprocess and all its
+      # descendants.
       for descendant in descendants:
         try:
           mem_info = descendant.memory_info()
 
           rss_memory += mem_info[0]
           vms_memory += mem_info[1]
-        except psutil.NoSuchProcess:
-          #sometimes a subprocess descendant will have terminated between the time
-          # we obtain a list of descendants, and the time we actually poll this
-          # descendant's memory usage.
+        except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.AccessDenied):
+          
+          # Sometimes a subprocess descendant will have terminated
+          # between the time we obtain a list of descendants, and the
+          # time we actually poll this descendant's memory usage.
           pass
       if int(self.max_vms_memory * 1E-8) < int(vms_memory * 1E-8):
-        # peak memory updated, at about 100MB resolution
+        
+        # Peak memory updated, at ~100-MB resolution.
         self.max_t = [self.t1]
       if int(self.max_vms_memory * 1E-8) == int(vms_memory * 1E-8):
-        # peak memory maintained
+        
+        # Peak memory maintained.
         self.max_t = [self.max_t[0], self.t1]
       self.max_vms_memory = max(self.max_vms_memory,vms_memory)
       self.max_rss_memory = max(self.max_rss_memory,rss_memory)
 
-    except psutil.NoSuchProcess:
+    except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.AccessDenied):
       return self.check_execution_state()
 
     return self.check_execution_state()
@@ -105,7 +112,7 @@ class ProcessTimer:
           pp.kill()
         else:
           pp.terminate()
-      except psutil.NoSuchProcess:
+      except (psutil.NoSuchProcess, psutil.ZombieProcess, psutil.AccessDenied):
         pass
 
 def takewhile_excluding(iterable, value = ['|', '<', '>']):
@@ -126,12 +133,15 @@ if __name__ == '__main__':
 
   try:
     ptimer.execute()
-    #poll as often as possible; otherwise the subprocess might
-    # "sneak" in some extra memory usage while you aren't looking
+    
+    # Poll as often as possible; otherwise the subprocess might
+    # "sneak" in some extra memory usage while you aren't looking.
     while ptimer.poll():
       time.sleep(ptimer.interval)
+      
   finally:
-    #make sure that we don't leave the process dangling?
+    
+    # Make sure that we don't leave the process dangling.
     ptimer.close()
 
   sys.stderr.write('\ntime elapsed: {:.2f}s\n'.format(max(0, ptimer.t1 - ptimer.t0 - ptimer.interval * 0.5)))
