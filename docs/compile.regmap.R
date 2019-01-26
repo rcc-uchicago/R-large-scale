4f8472d1cef1ad7bbe23c6b2835f5875263fceb7
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-26 16:08:59 -0600

    Working on revisions to compile.regmap.R script.

--- docs/compile.regmap.R
+++ docs/compile.regmap.R
@@ -1,39 +1,42 @@
 # Script to compile the RegMap data.
 #
-# Before running this script, download the RegMap data files from 
-# bergelson.uchicago.edu/wp-content/uploads/2015/04/call_method_75.tar.gz.
+# Before running this script, download the RegMap genotype data from here:
 #
-library(methods)
-library(readr)
-source("functions.R")
-
-# LOAD SAMPLE INFO
-# ----------------
-# Retrieve the following sample info: array id, ecotype id, genotyping
-# intensity, geographic co-ordinates (latitude and longitude), region
-# label, and country label. This should produce a data frame with
-# 1,307 rows (samples) and 6 columns.
+#   bergelson.uchicago.edu/wp-content/uploads/2015/04/call_method_75.tar.gz
 #
-# cat("Reading sample info.\n")
-# regmap.info <- read.regmap.info("../data/call_method_75_info.tsv")
+library(data.table)
+source("compile.regmap.functions.R")
 
 # LOAD PHENOTYPE DATA
 # -------------------
-# This should produce a data frame, "regmap.pheno" , with 948 rows
-# (samples) and 48 columns (phenotypes).
-#
-# cat("Reading phenotype data.\n")
-# regmap.pheno <- read.regmap.pheno("../data/allvars948_notnormd_011311.txt")
-  
+# This should produce a data frame with 948 rows (samples) and 48
+# columns (phenotypes).
+cat("Reading phenotype data.\n")
+pheno <- read.regmap.pheno("allvars948_notnormd_011311.txt")
+
 # LOAD GENOTYPE DATA
 # ------------------
 # Load the genotype data and information about the genetic markers.
 cat("Reading genotype data.\n")
-geno <- read.regmap.geno("call_method_75_TAIR9.csv")
+out     <- read.regmap.geno("call_method_75_TAIR9.csv")
+geno    <- out$geno
+markers <- out$markers
+
+# LOAD GENOTYPE SAMPLE INFO
+# -------------------------
+# Retrieve the following sample info: array id, ecotype id, genotyping
+# intensity, geographic co-ordinates (latitude and longitude), region
+# label, and country label. This should produce a data frame with
+# 1,307 rows (samples) and 6 columns.
+cat("Reading sample info.\n")
+info <- read.regmap.info("call_method_75_info.tsv")
+
 
 # Convert the genotype data to a binary matrix.
 cat("Converting genotype data to a binary matrix.\n")
-regmap.geno <- regmap.geno.as.binary(regmap.geno)
+geno <- regmap.geno.as.binary(geno)
+
+stop()
 
 # Reorder the rows of regmap.info so that they match up with the rows
 # of regmap.geno.
