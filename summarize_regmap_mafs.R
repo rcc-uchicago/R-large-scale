373437b662e054c823ef769abcf9318bfc1ccc2d
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-28 12:03:28 -0600

    Made a few improvements to summarize_regmap_mafs.R script.

--- summarize_regmap_mafs.R
+++ summarize_regmap_mafs.R
@@ -9,15 +9,19 @@ library(data.table)
 cat("Reading genotype data.\n")
 geno <- fread("geno.csv",sep = ",",header = TRUE)
 class(geno) <- "data.frame"
+n <- nrow(geno)
+p <- ncol(geno)
+cat(sprintf("Loaded %d x %d genotype matrix.\n",n,p))
 
 # COMPUTE MAFs
 # ------------
-cat("Computing minor allele frequencies.\n")
+cat(sprintf("Computing MAFs for %d SNPs.\n",p))
 maf <- sapply(geno,mean)
 maf <- pmin(maf,1 - maf)
 
 # SUMMARIZE RESULTS
 # -----------------
+cat(sprintf("Distribution of MAFs across %d SNPs:\n",p))
 print(summary(maf))
 
 # SESSION INFO
