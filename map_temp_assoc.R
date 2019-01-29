e0dec128c7956b347993e45687aaa376ee0ff538
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-29 10:43:41 -0600

    Made various improvements to the map_temp_assoc.R script.

---
+++ map_temp_assoc.R
@@ -0,0 +1,70 @@
+# A short script to compute
+
+# SET UP ENVIRONMENT
+# ------------------
+library(data.table)
+
+# IMPORT PHENOTYPE DATA
+# ---------------------
+# Retrieve the  
+cat("Reading phenotype data.\n")
+pheno <- read.csv("pheno.csv")$bio5_maxT_wm
+n     <- length(pheno)
+cat(sprintf("Loaded %d temperature measurements.\n",n))
+
+# IMPORT GENOTYPE DATA
+# --------------------
+cat("Reading genotype data.\n")
+geno        <- fread("geno.csv",sep = ",",header = TRUE)
+class(geno) <- "data.frame"
+n           <- nrow(geno)
+p           <- ncol(geno)
+cat(sprintf("Loaded %d x %d genotype matrix.\n",n,p))
+
+# Remove all SNPs that do not vary.
+cat("Filtering SNPs.\n")
+s    <- sapply(geno,sd)
+geno <- geno[s > 0]
+
+# COMPUTE ASSOCIATIONS
+# --------------------
+# For now, compute associations for the first 10,000 SNPs
+# only. Comment out this two lines to compute associations for all
+# available SNPs.
+p    <- 10000
+geno <- geno[1:p]
+
+# This function takes two inputs, the genotype (x) and the phenotype
+# (y), and returns the p-value indicating the support an association
+# between x and y.
+get.assoc.pvalue <- function (x, y)
+  summary(lm(y ~ x,data.frame(x = x,y = y)))$coefficients["x","Pr(>|t|)"]
+
+# This function will return an association p-value for each column of
+# the data frame "dat".
+get.assoc.pvalues <- function (dat, y)
+  sapply(dat,function (x) get.assoc.pvalue(x,y))
+
+# Compute the association p-values for all SNPs (that is, columns of
+# the "geno" data frame).
+cat(sprintf("Computing association p-values for %d SNPs.\n",p))
+timing <- system.time(pvalues <- get.assoc.pvalues(geno,pheno))
+print(timing)
+
+## library(parallel)
+## nc <- 8
+## cl <- makeCluster(nc)
+## clusterExport(cl,c("get.assoc.pvalue","get.assoc.pvalues"))
+## cols <- clusterSplit(cl,1:p)
+## timing <- system.time(
+##   out <- parLapply(cl,cols,
+##     function (i, geno, pheno) get.assoc.pvalues(geno[,i],pheno),
+##     geno, pheno))
+## pvalues <- rep(0,p)
+## pvalues[unlist(cols)] <- unlist(out)
+## stopCluster(cl)
+
+# SUMMARIZE ASSOCIATION RESULTS
+# -----------------------------
+cat(sprintf("Distribution of association p-values across %d SNPs:\n",p))
+print(quantile(pvalues,c(0,0.001,0.01,0.1,0.25,0.5,1)))
